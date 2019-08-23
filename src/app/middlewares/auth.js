d73fb4cf8367141a6130d7ec055fdbd3982ebb2a
tiago <tiagojosebogoni@gmail.com>
2019-08-22 22:54:23 -0300

    Adicionado login

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -16,7 +16,7 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
-    console.log(decoded);
+
     return next();
   } catch (err) {
     return res.status(401).json({ error: 'Token inválido' });
