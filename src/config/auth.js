c336bcdc3e0f1f916ee92b8e2c6049849ad1d8c1
tiago <tiagojosebogoni@gmail.com>
2019-08-26 14:08:44 -0300

    Adicionado funçao para listar os agendamentos do dia. Criado arquivo .env

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'fe870881842787e6c7379608b996b806',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '458f75721b004e',
-    pass: 'e2d8cd59a34659',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
