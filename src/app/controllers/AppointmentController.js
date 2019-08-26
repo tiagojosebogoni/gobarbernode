70320d60138ceb19736d53c3db1bfef726d197a6
tiago <tiagojosebogoni@gmail.com>
2019-08-25 22:19:07 -0300

    Adicionado funçao para gravar agendamento e listar todos os agendamentos do usuario

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,76 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import Appointment from '../models/Appointment';
+import File from '../models/File';
+import User from '../models/User';
+
+class AppointmentsController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
+  async store(req, res) {
+    const { provider_id, date } = req.body;
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Erro na validação dos campos' });
+    }
+
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider)
+      return res
+        .status(401)
+        .json({ error: 'Para agendar tem que ser um prestador' });
+
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date()))
+      return res
+        .status(400)
+        .json({ error: 'Somente agendamentos em datas futuras' });
+
+    const checkAvailability = await Appointment.findOne({
+      where: { provider_id, canceled_at: null, date: hourStart },
+    });
+
+    if (checkAvailability) {
+      res.status(401).json({ error: 'Data está ocupada' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentsController();
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,6 +21,9 @@ routes.use(authMiddleware);
 routes.put('/user', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointment', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
